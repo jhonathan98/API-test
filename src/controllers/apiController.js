7dc3ed7fd5a98462ca3bbe6749f239372a9a29e6
Cordoba <jhonathan.cordoba@softwareone.com>
2025-01-07 17:47:15 -0500

    danamic name file

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -25,8 +25,10 @@ const getFile = async (req, res) => {
 
 const uploadFileBucket = async (req, res) => {
     try {
-        const response = await uploadImageFromUrl(process.env.URL_IMG, process.env.AWS_BUCKET, "test1.png");
+        const nombrefile = process.env.URL_IMG.split('/')[process.env.URL_IMG.split('/').length-1]
+        const response = await uploadImageFromUrl(process.env.URL_IMG, process.env.AWS_BUCKET, nombrefile+".png");
         console.log("Archivo subido exitosamente",response);
+        res.json({ message: "Archivo subido exitosamente",response:response });
     } catch (error) {
         console.log("error en el controller",error);
         res.status(500).json({ message: "error durante la carga del archivo"});
