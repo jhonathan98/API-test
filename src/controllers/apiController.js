508cf24a135fbc379081ae158f76e216d379573c
Cordoba <jhonathan.cordoba@softwareone.com>
2025-01-14 13:50:26 -0500

    consume api with axios

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,6 +1,7 @@
 const { fetchDataCountries } = require('../services/apiService');
 const downloadFile = require('../utils/DonwloadFile.js');
 const uploadImageFromUrl = require('../utils/UploadFile.js');
+const axios = require('axios');
 
 const getDataCountries = async (req, res) => {
     try {
@@ -36,26 +37,43 @@ const uploadFileBucket = async (req, res) => {
 }
 
 const GetAllTiketHubSpot = async (req, res) => {
-    const hubspot = require('@hubspot/api-client');
+    // const hubspot = require('@hubspot/api-client');
 
-    const hubspotClient = new hubspot.Client({"accessToken":process.env.HUBSPOT_API_KEY});
-    console.log("hubspotClient",hubspotClient);
-    const limit = 10;
-    const after = undefined;
-    const properties = undefined;
-    const propertiesWithHistory = undefined;
-    const associations = undefined;
-    const archived = false;
+    // const hubspotClient = new hubspot.Client({"accessToken":process.env.HUBSPOT_API_KEY});
+    // console.log("hubspotClient",hubspotClient);
+    // const limit = 10;
+    // const after = undefined;
+    // const properties = undefined;
+    // const propertiesWithHistory = undefined;
+    // const associations = undefined;
+    // const archived = false;
 
+    // try {
+    // const apiResponse = await hubspotClient.crm.contacts.basicApi.getPage(limit, after, properties, propertiesWithHistory, associations, archived);
+    // console.log(JSON.stringify(apiResponse, null, 2));
+    // res.json(apiResponse);
+    // } catch (e) {
+    //     e.message === 'HTTP request failed'
+    //     ? console.error(JSON.stringify(e.response, null, 2))
+    //     : console.error(e);
+    //     res.status(500).json({ message: e.message, error: e });
+    // }
     try {
-    const apiResponse = await hubspotClient.crm.contacts.basicApi.getPage(limit, after, properties, propertiesWithHistory, associations, archived);
-    console.log(JSON.stringify(apiResponse, null, 2));
-    res.json(apiResponse);
-    } catch (e) {
-        e.message === 'HTTP request failed'
-        ? console.error(JSON.stringify(e.response, null, 2))
-        : console.error(e);
-        res.status(500).json({ message: e.message, error: e });
+        const response = await axios.get(`https://api.hubapi.com/crm/v3/objects/contacts`, {
+            headers: {
+                Authorization: `Bearer ${process.env.HUBSPOT_API_KEY}`, // Requerido para OAuth; reemplazar con `hapikey` para claves API
+                'Content-Type': 'application/json',
+            },
+            params: {
+                limit: 10//, // Número de contactos por página
+                //properties: 'firstname,lastname,email', // Campos a recuperar
+            },
+        });
+        console.log('Contactos obtenidos:', response.data.results);
+        res.json(response.data.results);
+    } catch (error) {
+        console.error('Error al obtener contactos:', error.response?.data || error.message);
+        res.status(500).json({ message: error});
     }
 }
 
