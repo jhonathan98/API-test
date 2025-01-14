525b27c042811fb63a1ff52a4396f85a141a6146
Cordoba <jhonathan.cordoba@softwareone.com>
2025-01-14 13:37:47 -0500

    add console for print hubspotClient

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -39,7 +39,7 @@ const GetAllTiketHubSpot = async (req, res) => {
     const hubspot = require('@hubspot/api-client');
 
     const hubspotClient = new hubspot.Client({"accessToken":process.env.HUBSPOT_API_KEY});
-
+    console.log("hubspotClient",hubspotClient);
     const limit = 10;
     const after = undefined;
     const properties = undefined;
